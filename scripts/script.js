4dbb516211986652c34da456bff26ab367d1eeab
Dora Kutsenko <kk.tasha@yandex.ru>
2022-07-15 17:06:02 +0300

    удалены файлы изменены ошибки

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,7 @@ const popup = document.querySelector('.popup'),
 popupToggle = document.querySelector('.profile__edit-button'),
 popupClose = document.querySelector('.popup__closed'),
 popupName = document.querySelector('.popup__input_name'),
-popupProfession = document.querySelector('.popup__input_profession'),
-popupSubmit = document.querySelector('.popup__submit-btn');
-
+popupProfession = document.querySelector('.popup__input_profession');
 const popupTitle = document.querySelector('.profile__name'),
 popupSubtitle = document.querySelector('.profile__text');
 //открыть
