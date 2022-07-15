40ccf4c3f656685e2ceed8010d814886987b7f3c
Dora Kutsenko <kk.tasha@yandex.ru>
2022-07-15 15:13:34 +0300

    удалены лишние файлы

--- scripts/cards.js
+++
@@ -1,8 +0,0 @@
-const samplesCards = [
-  { title: 'Карачаево-Черкесия', src: './images/mask1.png' },
-  { title: 'Гора Эльбрус', src: './images/mask2.png' },
-  { title: 'Домбай', src: './images/mask3.png' },
-  { title: 'Гора Эльбрус', src: './images/mask4.png' },
-  { title: 'Домбай', src: './images/mask5.png' },
-  { title: 'Карачаевск', src: './images/mask6.png' }
-]
